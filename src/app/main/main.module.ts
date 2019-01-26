ed52fe7521a46b1fe27a5263ac61e9b10175eea4
“Yuriy <yuriy.deneha@gmail.com>
2019-01-26 14:44:00 +0200

    create tabs for products

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -5,11 +5,15 @@ import { MainRoutingModule } from './main-routing.module';
 import { ItemsListComponent } from './components/items-list/items-list.component';
 import { ItemComponent } from './components/items-list/item/item.component';
 import { DataService } from './components/items-list/data.service';
+import {MatTabsModule} from '@angular/material';
+import {SharedModule} from '../shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    MainRoutingModule
+    MainRoutingModule,
+    MatTabsModule,
+    SharedModule
   ],
   declarations: [
     IndexComponent,
